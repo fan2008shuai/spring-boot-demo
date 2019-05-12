3c537249e11766f7d82c8d76c3968988c9fc8e12
fan.shuai <fan2008shuai@163.com>
2019-05-12 19:04:59 +0800

    update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.setProperty("server.port", "8090");
-
-        System.setProperty("project.name", "org-fan-sentinel-demo");
-        System.setProperty("csp.sentinel.dashboard.server", "localhost:8080");
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,13 +1,6 @@
 package hello;
 
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
-
-import com.alibaba.csp.sentinel.Entry;
-import com.alibaba.csp.sentinel.EntryType;
-import com.alibaba.csp.sentinel.SphU;
-import com.alibaba.csp.sentinel.Tracer;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,39 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String RESOURCE_NAME = "org-fan-sentinel-demo-resource";
-
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        Entry entry = null;
-        Greeting greeting = null;
-
-        try {
-            entry = SphU.entry(RESOURCE_NAME, EntryType.IN);
-            greeting = new Greeting(counter.incrementAndGet(),
-                    String.format(template, name));
-
-            if (new Random().nextInt(2) < 1) {
-                throw new RuntimeException("except");
-            }
-
-        } catch (BlockException e) {
-            System.out.println("blocked exception");
-            greeting = new Greeting(counter.incrementAndGet(),
-                    String.format(template, "blocked"));
-        } catch (Exception e) {
-            Tracer.trace(e);
-            System.out.println("runtime exception");
-            greeting = new Greeting(counter.incrementAndGet(),
-                    String.format(template, "runtime exception"));
-        } finally {
-            if (entry != null) {
-                entry.exit();
-            }
-        }
-        return greeting;
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
     }
 }
